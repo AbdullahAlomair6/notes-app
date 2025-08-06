9d2a82865c09718d84c1e21bf776f4613dae0a23
Abdullah <abdullah.alomair@globant.com>
2025-08-06 04:27:48 +0300

    fix routes

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,31 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:notes_app/modules/login/view/login_screen.dart';
 import 'package:notes_app/modules/login/view/signup_screen.dart';
-
 import '../layout/home_layout.dart';
 
 enum AppRouter { homeLayout, loginScreen, signupScreen }
 
 GoRouter goRouter() {
   return GoRouter(
-    initialLocation: '/signupScreen',
+    initialLocation: '/loginScreen',
     routes: <RouteBase>[
       GoRoute(
         path: "/homeLayout",
         name: AppRouter.homeLayout.name,
-        builder: (context, state) => HomeLayout(),
+        builder: (context, state) => const HomeLayout(),
       ),
       GoRoute(
         path: "/loginScreen",
         name: AppRouter.loginScreen.name,
-        builder: (context, state) => LoginScreen(),
+        builder: (context, state) => const LoginScreen(),
       ),
       GoRoute(
         path: "/signupScreen",
         name: AppRouter.signupScreen.name,
-        builder: (context, state) => SignupScreen(),
+        builder: (context, state) => const SignupScreen(),
       ),
     ],
+    redirect: (context, state) {
+      final user = FirebaseAuth.instance.currentUser;
+      final loggingIn = state.matchedLocation == '/loginScreen' || state.matchedLocation == '/signupScreen';
+
+      if (user == null && !loggingIn) {
+        return '/loginScreen';
+      } else if (user != null && loggingIn) {
+        return '/homeLayout';
+      }
+      return null;
+    },
   );
 }
